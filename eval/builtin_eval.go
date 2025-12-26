4699ad6156af60a6c6c6b89b77388e182b65d69f
Christopher Toth <q.alpha@gmail.com>
2025-12-25 20:59:38 -0700

    Fix eval() to accept multiple string arguments joined with newlines

--- eval/builtin_eval.go
+++ eval/builtin_eval.go
@@ -2,22 +2,29 @@ package eval
 
 import (
 	"barn/types"
+	"strings"
 )
 
 // RegisterEvalBuiltin registers the eval() builtin function
 // This must be called from the evaluator after the builtins registry is created
 func (e *Evaluator) RegisterEvalBuiltin() {
 	e.builtins.Register("eval", func(ctx *types.TaskContext, args []types.Value) types.Result {
-		if len(args) != 1 {
+		if len(args) < 1 {
 			return types.Err(types.E_ARGS)
 		}
 
-		strVal, ok := args[0].(types.StrValue)
-		if !ok {
-			return types.Err(types.E_TYPE)
+		// All arguments must be strings
+		var lines []string
+		for _, arg := range args {
+			strVal, ok := arg.(types.StrValue)
+			if !ok {
+				return types.Err(types.E_TYPE)
+			}
+			lines = append(lines, strVal.Value())
 		}
 
-		code := strVal.Value()
+		// Join with newlines
+		code := strings.Join(lines, "\n")
 
 		// Use the evaluator's EvalString method
 		result := e.EvalString(code, ctx)
