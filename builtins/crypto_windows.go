ec0945f446dff0d6367a37fa7278e04be65aac2e
Christopher Toth <q.alpha@gmail.com>
2026-02-11 12:35:41 -0700

    Switch DES crypt to digitive/crypt for correct hash output
    The sergeymakinen/go-crypt library produced incorrect DES hashes.
    The digitive/crypt library provides a standard Unix crypt(3) that
    matches Toast's password verification.

--- builtins/crypto_windows.go
+++ builtins/crypto_windows.go
@@ -4,24 +4,15 @@
 package builtins
 
 import (
-	"github.com/sergeymakinen/go-crypt/des"
-	crypthash "github.com/sergeymakinen/go-crypt/hash"
+	"github.com/digitive/crypt"
 )
 
 // cryptDESPlatform implements traditional Unix DES crypt on Windows
-// Uses pure Go implementation from github.com/sergeymakinen/go-crypt
+// Uses pure Go implementation from github.com/digitive/crypt
 func cryptDESPlatform(password, salt string) (string, error) {
-	// Get the 8-byte key from the DES algorithm
-	key, err := des.Key([]byte(password), []byte(salt))
-	if err != nil {
-		return "", err
+	// Extract 2-char salt from potentially longer input (stored hash)
+	if len(salt) > 2 {
+		salt = salt[:2]
 	}
-
-	// Encode the key using the same encoding as the library
-	// The sum is 11 characters of the hash
-	var sum [11]byte
-	crypthash.BigEndianEncoding.Encode(sum[:], key)
-
-	// Return salt (2 chars) + hash (11 chars) = 13 chars total
-	return salt + string(sum[:]), nil
+	return crypt.Crypt(password, salt)
 }
--- go.mod
+++ go.mod
@@ -7,3 +7,5 @@ require (
 	golang.org/x/crypto v0.46.0
 	gopkg.in/yaml.v3 v3.0.1
 )
+
+require github.com/digitive/crypt v0.2.0 // indirect
